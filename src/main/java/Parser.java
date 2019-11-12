681a58990e904bbc17de9a77480899d680e7bff4
Michał Czarnecki <czarnecky@va.pl>
2019-11-12 23:41:52 +0100

    Obsługa wyjątku

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,6 +3,7 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,12 +14,18 @@ public class Parser {
     static void fillImports(List<JavaFile> files) {
         if (files == null) return;
         for (JavaFile jf : files) {
-            CompilationUnit cu = StaticJavaParser.parse(jf.getContent());
+            String content;
+            try {
+                content = jf.getContent();
+            } catch (FileNotFoundException e) {
+                continue;
+            }
+            CompilationUnit cu = StaticJavaParser.parse(content);
             List<JavaFile> imports = new ArrayList<>();
             cu.findAll(ImportDeclaration.class).stream()
                     .forEach(i -> {
                         // TODO ktoś musi "przetłumaczyć" import na ścieżkę
-                        imports.add(new JavaFile(new File("./path/to/"+i.getName())));
+                        imports.add(new JavaFile(new File("./secret-path/"+i.getName())));
                     });
             jf.setImports(imports);
         }
@@ -26,6 +33,6 @@ public class Parser {
 
     /* Pokazówka */
     public static void main(String[] args) {
-        Parser.fillImports(JavaFile.getFilesFrom("."));
+        Parser.fillImports(JavaFile.getFilesFrom(JavaFile.getProjectPath()));
     }
 }
