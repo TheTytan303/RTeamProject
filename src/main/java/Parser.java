75dd5f4b4fe0052533f75c88678aaa693bc1b795
Michał Czarnecki <czarnecky@va.pl>
2019-11-12 23:09:59 +0100

    Dodać Parser i libkę - tym razem na serio
    + Pierwsza implementacja Parsera

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,4 +1,26 @@
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.ImportDeclaration;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class Parser {
 
+    static void fillImports(List<JavaFile> files) {
+        if (files == null) return;
+        for (JavaFile jf : files) {
+            CompilationUnit cu = StaticJavaParser.parse(jf.getContent());
+            cu.findAll(ImportDeclaration.class).stream()
+                    .forEach(i -> {
+                        System.out.println(i.getName());
+                    });
+        }
+    }
+
+    /* Pokazówka */
+    public static void main(String[] args) {
+        Parser.fillImports(JavaFile.getFilesFrom("."));
+    }
 }
