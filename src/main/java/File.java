19de60b041eb464d4c7a257f42e1bb75b24fd4d8
TheTytan303 <wciesla303@gmail.com>
2019-11-12 22:15:48 +0100

    File class prototype

---
+++ src/main/java/File.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+
+public class File {
+    String path;
+
+    //poierz ścieżkę pliku
+    public String getPath(){
+        return null;
+    }
+
+    //pobierz zawartość pliku
+    public String getContent(){
+        return null;
+    }
+
+    //pobierz rozmiar pliku
+    public BigInteger getSize(){
+        return null;
+    }
+}
