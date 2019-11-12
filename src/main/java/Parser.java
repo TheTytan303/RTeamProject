f7e521321790915f4f0e7dead6ee3c5d00cd4a70
Michał Czarnecki <czarnecky@va.pl>
2019-11-12 23:27:34 +0100

    Uzupełnianie pola imports

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -2,7 +2,9 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 
+import java.io.File;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,10 +14,13 @@ public class Parser {
         if (files == null) return;
         for (JavaFile jf : files) {
             CompilationUnit cu = StaticJavaParser.parse(jf.getContent());
+            List<JavaFile> imports = new ArrayList<>();
             cu.findAll(ImportDeclaration.class).stream()
                     .forEach(i -> {
-                        System.out.println(i.getName());
+                        // TODO ktoś musi "przetłumaczyć" import na ścieżkę
+                        imports.add(new JavaFile(new File("./path/to/"+i.getName())));
                     });
+            jf.setImports(imports);
         }
     }
 
