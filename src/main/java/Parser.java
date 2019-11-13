849a5dde62d5f7431e73c2862447071f3ab94d55
Michał Czarnecki <czarnecky@va.pl>
2019-11-13 12:56:06 +0100

    Zmiana parsera tak, aby rozróżniał paczkę od importowanej klasy

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,38 +1,46 @@
+//package io.github.TheTytan303.RTeamProject;
+
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.expr.Name;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Parser {
 
-    static void fillImports(List<JavaFile> files) {
-        if (files == null) return;
-        for (JavaFile jf : files) {
-            String content;
-            try {
-                content = jf.getContent();
-            } catch (FileNotFoundException e) {
-                continue;
-            }
-            CompilationUnit cu = StaticJavaParser.parse(content);
-            List<JavaFile> imports = new ArrayList<>();
-            cu.findAll(ImportDeclaration.class).stream()
-                    .forEach(i -> {
-                        // TODO ktoś musi "przetłumaczyć" import na ścieżkę
-                        imports.add(new JavaFile(new File("./secret-path/"+i.getName())));
-                    });
-            jf.setImports(imports);
+    public static Set<Import> getImports(JavaFile jf) {
+        Set<Import> imports = new HashSet<>();
+        String content;
+        try {
+            content = jf.getContent();
+        } catch (FileNotFoundException e) {
+            return imports;
         }
+        CompilationUnit cu = StaticJavaParser.parse(content);
+        for (ImportDeclaration id : cu.getImports())  {
+            Name importName = id.getName();
+            String fullImport = importName.asString();
+            Optional<Name> qualifier = importName.getQualifier();
+            if (qualifier.isPresent()) {
+                String fromPackage = qualifier.get().asString();
+                String importedClass = importName.removeQualifier().asString();
+                imports.add(new Import(fromPackage, importedClass));
+            } else {
+                imports.add(new Import(fullImport));
+            }
+        }
+        return imports;
     }
 
     /* Pokazówka */
     public static void main(String[] args) {
-        Parser.fillImports(JavaFile.getFilesFrom(JavaFile.getProjectPath()));
+        for (JavaFile jf : JavaFile.getFilesFrom(JavaFile.getProjectPath())) {
+            Set<Import> im = Parser.getImports(jf);
+            for (Import i : im) {
+                System.out.println(i);
+            }
+        }
     }
 }
