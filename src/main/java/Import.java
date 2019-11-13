e22da9de2981ab7994ea3b8cafcce813453b35d8
Michał Czarnecki <czarnecky@va.pl>
2019-11-13 12:55:09 +0100

    Dodanie klasy Import

---
+++ src/main/java/Import.java
@@ -0,0 +1,30 @@
+public class Import {
+    private String importedPackage;
+    private String importedClass;
+
+    public Import(String importedPackage) {
+        this.importedPackage = importedPackage;
+    }
+
+    public Import(String importedPackage, String importedClass) {
+        this.importedPackage = importedPackage;
+        this.importedClass = importedClass;
+    }
+
+    public String getImportedPackage() {
+        return importedPackage;
+    }
+
+    public String getImportedClass() {
+        return importedClass;
+    }
+
+    @Override
+    public String toString() {
+        if (importedClass == null || importedClass.equals("")) {
+            return importedPackage + ".*";
+        } else {
+            return importedPackage + '.' + importedClass;
+        }
+    }
+}
