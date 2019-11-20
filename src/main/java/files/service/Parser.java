bb2b655f8de698df65fd5a86e4efe3e79e585fce
Michał Czarnecki <czarnecky@va.pl>
2019-11-20 18:04:33 +0100

    Add call_graph() and rename graph() to package_graph()

--- src/main/java/files/service/Parser.java
+++ src/main/java/files/service/Parser.java
@@ -4,12 +4,29 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
 import com.github.javaparser.ast.PackageDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.Name;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import files.model.JavaFile;
 import files.model.PackageFile;
 
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
+
+class MCV extends VoidVisitorAdapter<Set<String>> {
+    @Override
+    public void visit(MethodCallExpr mce, Set<String> m) {
+        if (mce.getScope().isPresent()) {
+            m.add(mce.getScope().get() + "." + mce.getName().asString());
+        } else {
+            m.add(mce.getName().asString());
+        }
+        super.visit(mce, m);
+    }
+}
 
 public class Parser {
 
@@ -51,7 +68,7 @@ public class Parser {
         return imports;
     }
 
-    private static Map<String, Set<String>> _graph(PackageFile pf, Map<String, Set<String>> g) {
+    private static Map<String, Set<String>> _package_graph(PackageFile pf, Map<String, Set<String>> g) {
         for (JavaFile jf : pf.getJavaFiles()) {
             Set<Import> im;
             String key;
@@ -72,7 +89,32 @@ public class Parser {
             }
         }
         for (PackageFile spf : pf.getPackages()) {
-            _graph(spf, g);
+            _package_graph(spf, g);
+        }
+        return g;
+    }
+
+    private static Map<String, Set<String>> _call_graph(PackageFile pf, Map<String, Set<String>> g) {
+        for (JavaFile jf : pf.getJavaFiles()) {
+            String content;
+            try {
+                content = jf.getContent();
+            } catch (FileNotFoundException e) {
+                continue;
+            }
+            CompilationUnit cu = StaticJavaParser.parse(content);
+            for (ClassOrInterfaceDeclaration coi : cu.findAll(ClassOrInterfaceDeclaration.class).stream()
+                    .filter(co-> !co.isInterface())
+                    .collect(Collectors.toSet())) {
+                Set<String> calls = new HashSet<>();
+                for (MethodDeclaration md : coi.getMethods()) {
+                    md.accept(new MCV(), calls);
+                }
+                g.put(coi.getName().asString(), calls);
+            }
+        }
+        for (PackageFile spf : pf.getPackages()) {
+            _call_graph(spf, g);
         }
         return g;
     }
@@ -82,16 +124,29 @@ public class Parser {
      * @param path directory to be scanned
      * @return Mapping
      */
-    public static Map<String, Set<String>> graph(String path) {
+    public static Map<String, Set<String>> package_graph(String path) {
         Map<String, Set<String>> g = new HashMap<>();
-        return _graph(new PackageFile(path), g);
+        return _package_graph(new PackageFile(path), g);
+    }
+
+    public static Map<String, Set<String>> call_graph(String path) {
+        Map<String, Set<String>> g = new HashMap<>();
+        return _call_graph(new PackageFile(path), g);
     }
 
     public static void main(String[] args) {
-        Map<String, Set<String>> g = graph(JavaFile.getProjectPath());
+        Map<String, Set<String>> g = package_graph(JavaFile.getProjectPath());
         for (String k : g.keySet()) {
             System.out.println(String.format("'%s' imports: (%d)", k, g.get(k).size()));
             for (String v : g.get(k)) {
+                System.out.println(String.format("\t%s", v));
+            }
+        }
+
+        Map<String, Set<String>> c = call_graph(JavaFile.getProjectPath());
+        for (String k : c.keySet()) {
+            System.out.println(String.format("'%s' calls: (%d)", k, c.get(k).size()));
+            for (String v : c.get(k)) {
                 System.out.println(String.format("\t%s", v));
             }
         }
