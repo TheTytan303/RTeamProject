eb558619d2c6f02e0dc2dfa8178356e01c26e8af
Witold Dankiewicz <48772051+wonziak@users.noreply.github.com>
2020-01-19 00:42:45 +0100

    UI update

--- src/main/java/Graph/Graph.java
+++ src/main/java/Graph/Graph.java
@@ -9,14 +9,18 @@ import org.jgrapht.graph.*;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Graph extends JApplet
 {
     private static final ArrayList<String> vs = new ArrayList<String>();
     private static final ArrayList<String>is = new ArrayList<String>();
     private static final ArrayList<Relationship> relationships = new ArrayList<>();
+    private static final ArrayList<String> vs2 = new ArrayList<String>();
+    private static final ArrayList<String>is2 = new ArrayList<String>();
+    private static final ArrayList<Relationship> relationships2 = new ArrayList<>();
     double scale=1.0;
-    private static final Dimension DEFAULT_SIZE = new Dimension(1900, 1000);
+    private static final Dimension DEFAULT_SIZE = new Dimension(500, 500);
     private JGraphXAdapter<String, RelationshipEdge> jgxAdapter;
     public Graph(double scale){
 
@@ -28,9 +32,12 @@ public class Graph extends JApplet
         this.init();
         panel.add(this);
 
+    }
+    public final void draw2(JPanel panel){
+        this.init2();
+        panel.add(this);
 
     }
-
     public final ArrayList[] importData(ArrayList<String> fileName, ArrayList<Long> fileSize, ArrayList<Relationship> relationship) throws IllegalArgumentException{
 
         if(fileName.isEmpty()|| fileSize.isEmpty() || relationship.isEmpty()) {
@@ -73,17 +80,85 @@ public class Graph extends JApplet
         return new ArrayList[]{vs, is, relationships};
 
     }
+    public final ArrayList[] importData2(ArrayList<String> fileName, ArrayList<Long> fileSize, ArrayList<Relationship> relationship,ArrayList<String> fileName2, ArrayList<Long> fileSize2, ArrayList<Relationship> relationship2) throws IllegalArgumentException{
 
+        if(fileName.isEmpty()|| fileSize.isEmpty() || relationship.isEmpty()||fileName2.isEmpty()|| fileSize2.isEmpty() || relationship2.isEmpty()) {
+            throw new  IllegalArgumentException("No files to load");
+        }
+        vs.clear();
+        is.clear();
+        vs2.clear();
+        is2.clear();
+        for(int i = 0; i < fileName.size(); i++) {
+            vs.add(fileName.get(i) + "\n" + fileSize.get(i).toString());
+            is.add("1");
+            relationships.add(relationship.get(i));
+        }
+        for(int i = 0; i < fileName2.size(); i++) {
+            vs2.add(fileName2.get(i) + "\n" + fileSize2.get(i).toString());
+            is2.add("1");
+            relationships2.add(relationship2.get(i));
+        }
+        //calculate relationships count
+        for(Relationship rel : relationships) {
+            String name = rel.getName();
+
+            //find vs with this name
+            System.out.println("FROM: " + name);
+
+            Integer outCount = 0;
+            for(int i=0; i<rel.getDependencies().size(); i++){
+                outCount++;
+
+                String depName = rel.getDependencies().get(i);
+                System.out.println("DEP:\t" + depName);
+
+                Integer pos = Relationship.getIndexFromName(relationships, rel.getDependencies().get(i));
+                if(pos != -1) {
+                    relationships.get(pos).incrementInCount();
+                    pos++;
+                }
+            }
+            rel.addOutCount(outCount);
+
+            System.out.println("\n\n");
+        }
+
+        for(Relationship rel : relationships2) {
+            String name = rel.getName();
+
+            //find vs with this name
+            //System.out.println("FROM: " + name);
+
+            Integer outCount = 0;
+            for(int i=0; i<rel.getDependencies().size(); i++){
+                outCount++;
+
+                String depName = rel.getDependencies().get(i);
+                System.out.println("DEP:\t" + depName);
+
+                Integer pos = Relationship.getIndexFromName(relationships, rel.getDependencies().get(i));
+                if(pos != -1) {
+                    relationships.get(pos).incrementInCount();
+                    pos++;
+                }
+            }
+            rel.addOutCount(outCount);
+
+            System.out.println("\n\n");
+        }
+
+        return new ArrayList[]{vs, is, relationships,relationships2,vs2,is2,};
+
+    }
     @Override
 
     public void init()
     {
         // create a JGraphT graph
         ListenableGraph<String, RelationshipEdge> g = new DefaultListenableGraph<>(new DefaultDirectedGraph<>(RelationshipEdge.class));
-
         // create a visualization using JGraph, via an adapter
         jgxAdapter = new JGraphXAdapter<>(g);
-
         setPreferredSize(DEFAULT_SIZE);
         mxGraphComponent component = new mxGraphComponent(jgxAdapter);
 
@@ -95,7 +170,6 @@ public class Graph extends JApplet
         resize(DEFAULT_SIZE);
 
         //jgxAdapter.getStylesheet().getDefaultEdgeStyle().put(mxConstants.STYLE_NOLABEL,"0");
-
         //add vertices
         for(Relationship rel : relationships) {
             String name = rel.getName();
@@ -113,12 +187,72 @@ public class Graph extends JApplet
         }
 
         // positioning via JGraphX layouts
-        mxCircleLayout layout = new mxCircleLayout(jgxAdapter);
 
+        mxCircleLayout layout = new mxCircleLayout(jgxAdapter);
         // center the circle
         int radius = 150;
-        layout.setX0((DEFAULT_SIZE.width / 2.0) - radius);
-        layout.setY0((DEFAULT_SIZE.height / 2.0) - radius);
+        layout.setX0(DEFAULT_SIZE.width  - radius);
+        layout.setY0(DEFAULT_SIZE.height  - radius);
+        layout.setRadius(radius);
+        layout.setMoveCircle(true);
+
+
+        mxGraphView view = layout.getGraph().getView();
+        view.setScale(scale);
+        layout.execute(jgxAdapter.getDefaultParent());
+
+
+
+    }
+    public void init2()
+    {
+        // create a JGraphT graph
+        ListenableGraph<String, RelationshipEdge> g = new DefaultListenableGraph<>(new DefaultDirectedGraph<>(RelationshipEdge.class));
+        // create a visualization using JGraph, via an adapter
+        jgxAdapter = new JGraphXAdapter<>(g);
+        setPreferredSize(DEFAULT_SIZE);
+        mxGraphComponent component = new mxGraphComponent(jgxAdapter);
+        component.setConnectable(false);
+        component.getGraph().setAllowDanglingEdges(false);
+
+        getContentPane().add(component);
+        resize(DEFAULT_SIZE);
+
+        //jgxAdapter.getStylesheet().getDefaultEdgeStyle().put(mxConstants.STYLE_NOLABEL,"0");
+
+        //add vertices
+        for(Relationship rel : relationships) {
+            String name = rel.getName();
+            g.addVertex(name);
+        }
+
+        for(Relationship rel : relationships2) {
+            String name = rel.getName();
+            g.addVertex(name);
+        }
+        //Add edges
+        for(Relationship rel : relationships) {
+            String name = rel.getName();
+
+            for(int i=0; i<rel.getDependencies().size(); i++){
+                String depName = rel.getDependencies().get(i);
+                g.addEdge(name, depName, new RelationshipEdge("IN:" + rel.getInCount() + ", OUT: " + rel.getOutCount()));
+            }
+        }
+        for(Relationship rel : relationships2) {
+            String name = rel.getName();
+
+            for(int i=0; i<rel.getDependencies().size(); i++){
+                String depName = rel.getDependencies().get(i);
+                g.addEdge(name, depName, new RelationshipEdge("IN:" + rel.getInCount() + ", OUT: " + rel.getOutCount()));
+            }
+        }
+        // positioning via JGraphX layouts
+       mxCircleLayout layout = new mxCircleLayout(jgxAdapter);
+        // center the circle
+        int radius = 150;
+        layout.setX0(DEFAULT_SIZE.width  - radius);
+        layout.setY0(DEFAULT_SIZE.height  - radius);
         layout.setRadius(radius);
         layout.setMoveCircle(true);
 
--- src/main/java/Rteam/RTeam.java
+++ src/main/java/Rteam/RTeam.java
@@ -9,6 +9,7 @@ import files.model.JavaFileContent.PredefinedJavaClass;
 import files.model.PackageFile;
 
 import files.service.ClassDeclaration;
+import files.service.GitInfo;
 import files.service.Parser;
 
 import javax.swing.*;
@@ -26,30 +27,46 @@ public class RTeam {
 
  public static JFrame frame = new JFrame();
  public static JPanel panel=new JPanel();
+
  static double scale=1.0;
+ static String version= GitInfo.getHeadHash(".");
+ static int storyActive=0;
 
     public static void frameInit(String title){
+
         frame.setTitle(title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel.setLayout(new BorderLayout());
+        panel.setLayout(new BorderLayout());;
         frame.getContentPane().add(panel);
+
         JMenuBar mb = new JMenuBar();
+        JLabel scaleLabel=new JLabel("scale: "+Double.toString(scale));
+        JLabel versionLabel=new JLabel("  version: "+version);
         JButton  saveMenuItem = new JButton ("Export");
         JButton  story1=new JButton ("Story 1");
         JButton  story2=new JButton ("Story 2");
+        JButton  story3=new JButton ("Story 3");
+        JButton  rem=new JButton ("Remove");
         JButton  scaleIncrease =new JButton ("Scale++");
         JButton  scaleDecrease =new JButton ("Scale--");
         mb.add(saveMenuItem);
         mb.add(story1);
         mb.add(story2);
+        mb.add(story3);
+        mb.add(rem);
         mb.add(scaleIncrease);
         mb.add(scaleDecrease);
+        mb.add(scaleLabel);
+        mb.add(versionLabel);
+
         story1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 panel.removeAll();
                 story1(scale,panel);
+                SwingUtilities.updateComponentTreeUI(frame);
+               storyActive=1;
             }
         });
         story2.addActionListener(new ActionListener() {
@@ -57,6 +74,25 @@ public class RTeam {
             public void actionPerformed(ActionEvent e) {
                 panel.removeAll();
                 story2(scale,panel);
+             SwingUtilities.updateComponentTreeUI(frame);
+             storyActive=2;
+            }
+        });
+        story3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panel.removeAll();
+                story3(scale,panel);
+                SwingUtilities.updateComponentTreeUI(frame);
+                storyActive=3;
+            }
+        });
+        rem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panel.removeAll();
+                SwingUtilities.updateComponentTreeUI(frame);
+                storyActive=0;
             }
         });
         scaleIncrease.addActionListener(new ActionListener() {
@@ -64,6 +100,27 @@ public class RTeam {
             public void actionPerformed(ActionEvent e) {
                 scale+=0.1;
                 System.out.println(scale);
+                scaleLabel.setText("scale: "+Double.toString(scale).substring(0,3));
+                switch (storyActive){
+                    case 1:
+                        panel.removeAll();
+                        story1(scale,panel);
+                        System.out.println(scale);
+                        SwingUtilities.updateComponentTreeUI(frame);
+                        break;
+                    case 2:
+                        panel.removeAll();
+                        story2(scale,panel);
+                        System.out.println(scale);
+                        SwingUtilities.updateComponentTreeUI(frame);
+                    break;
+                    case 3:
+                        panel.removeAll();
+                        story3(scale,panel);
+                        System.out.println(scale);
+                        SwingUtilities.updateComponentTreeUI(frame);
+                        break;
+                }
             }
         });
         scaleDecrease.addActionListener(new ActionListener() {
@@ -71,7 +128,28 @@ public class RTeam {
             public void actionPerformed(ActionEvent e) {
                 if(scale-0.1>0.1) {
                     scale -= 0.1;
+                    scaleLabel.setText("scale: "+Double.toString(scale).substring(0,3));
                     System.out.println(scale);
+                    switch (storyActive){
+                        case 1:
+                            panel.removeAll();
+                            story1(scale,panel);
+                            System.out.println(scale);
+                            SwingUtilities.updateComponentTreeUI(frame);
+                            break;
+                        case 2:
+                            panel.removeAll();
+                            story2(scale,panel);
+                            System.out.println(scale);
+                            SwingUtilities.updateComponentTreeUI(frame);
+                            break;
+                        case 3:
+                            panel.removeAll();
+                            story3(scale,panel);
+                            System.out.println(scale);
+                            SwingUtilities.updateComponentTreeUI(frame);
+                            break;
+                    }
                 }
             }
         });
@@ -158,6 +236,75 @@ public class RTeam {
             Graph applet = new Graph(scale);
             applet.importData(methodName, methodCallCount, relationships);
             applet.draw(panel);
+    }
+    public static void story3(double scale,JPanel panel){
+        PackageFile pack2 = new PackageFile(JavaFile.getProjectPath());
+
+        ArrayList<String> methodName2 = new ArrayList<>();
+        ArrayList<Long> methodCallCount2 = new ArrayList<>();
+        ArrayList<Relationship> relationships2 = new ArrayList<>();
+
+        List<JavaFile> files2 =pack2.getSubFiles();
+        List<JavaMethod> methods2= new ArrayList<>();
+
+        for(JavaFile jf: files2){
+            methods2.addAll(jf.getMethods());
+        }
+
+        for (JavaMethod jm : methods2) {
+            Map<JavaMethod, Integer> calledMethods = jm.getCalledMethod();
+
+            Iterator<Map.Entry<JavaMethod, Integer>> itr = calledMethods.entrySet().iterator();
+
+            while(itr.hasNext()) {
+                Map.Entry<JavaMethod, Integer> entry = itr.next();
+
+                if(entry.getKey() != null) {
+                    methodName2.add(entry.getKey().getClassMethodName());
+                    methodCallCount2.add((long) entry.getValue());
+                    ArrayList<String> calls = new ArrayList<>();
+
+                    Map<JavaMethod, Integer> innerCalls = entry.getKey().getCalledMethod();
+                    Iterator<Map.Entry<JavaMethod, Integer>> innerItr = innerCalls.entrySet().iterator();
+
+                    while(innerItr.hasNext()) {
+                        Map.Entry<JavaMethod, Integer> call = innerItr.next();
+                        if(call.getKey() != null) {
+                            calls.add(call.getKey().getClassMethodName());
+                        }
+                    }
+
+                    relationships2.add(new Relationship(entry.getKey().getClassMethodName(), calls));
+                }
+            }
+        }
+        PackageFile pack = new PackageFile(JavaFile.getProjectPath());
+        List<JavaClass> classes = pack.getSubClasses();
+        List<JavaFile> files = pack.getSubFiles();
+        Map<JavaMethod, Integer> map = classes.get(3).getMethodsCall();
+
+        ArrayList<String> fileName = new ArrayList<>();
+        ArrayList<Long> fileSize = new ArrayList<>();
+        ArrayList<Relationship> relationships = new ArrayList<>();
+        try {
+            for (JavaFile jf : pack.getSubFiles()) {
+                fileName.add(jf.getName());
+                fileSize.add(jf.getSize());
+
+                ArrayList<String> relationshipNames = new ArrayList<>();
+                for(JavaFile a : jf.getImports()) {
+                    relationshipNames.add(a.getName());
+                }
+                relationships.add(new Relationship(jf.getName(), relationshipNames));
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Graph applet = new Graph(scale);
+        applet.importData2(fileName, fileSize, relationships,methodName2, methodCallCount2, relationships2);
+        applet.draw2(panel);
     }
 
     public static void main(String[] args){
